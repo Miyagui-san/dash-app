5aed9abfdb5f464a3aa087920b6014704d0fc9b9
Miyagui-san <88814172+Miyagui-san@users.noreply.github.com>
2024-08-21 00:36:05 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import dash
+from dash import dcc, html
+import pandas as pd
+import plotly.express as px
+
+# Sample data
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Grapes"],
+    "Amount": [4, 1, 2, 3],
+    "City": ["SF", "SF", "SF", "SF"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0')
