db809439d12de42754aa1de3043b0aac8e7e5960
Miyagui-san <88814172+Miyagui-san@users.noreply.github.com>
2024-08-23 16:38:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 # Sample data
 df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Grapes123456"],
+    "Fruit": ["Apples", "Oranges", "Bananas", "Grapes"],
     "Amount": [4, 1, 2, 3],
     "City": ["SF", "SF", "SF", "SF"]
 })
